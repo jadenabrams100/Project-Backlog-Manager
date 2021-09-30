11562a556a5f36fa9914af2d07681be5cc842bf9
Jaden Abrams <jlabrams@ncsu.edu>
2021-09-29 22:12:05 -0400

    Created JUnit test skeletons

---
+++ Project1/test/edu/ncsu/csc216/product_backlog/model/io/ProductsReaderTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.product_backlog.model.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductsReaderTest {
+
+	@Test
+	void testReadProductsFile() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Project1/test/edu/ncsu/csc216/product_backlog/model/io/ProductsWriterTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.product_backlog.model.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductsWriterTest {
+
+	@Test
+	void testWriteProductsToFile() {
+		fail("Not yet implemented");
+	}
+
+}
