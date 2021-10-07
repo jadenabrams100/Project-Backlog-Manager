05816b6cf814f27ea4753c1537940704ffcbfbf0
Jaden Abrams <jlabrams@ncsu.edu>
2021-10-06 22:21:15 -0400

    [Implementation] fixed name setting in Task.RejectedState

--- Project1/src/edu/ncsu/csc216/product_backlog/model/task/Task.java
+++ Project1/src/edu/ncsu/csc216/product_backlog/model/task/Task.java
@@ -661,7 +661,7 @@ public class Task {
 			if(c.getCommand() == Command.CommandValue.BACKLOG) {
 				currentState = backlogState;
 				addNoteToList(c.getNoteText());
-				setOwner(null);
+				setOwner(UNOWNED);
 			}
 			else {
 				throw new UnsupportedOperationException("Invalid transition.");
