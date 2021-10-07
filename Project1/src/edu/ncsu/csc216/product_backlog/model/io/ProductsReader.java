320e93a14463cfab0ef6024aeba702e2ff12fae0
Jaden Abrams <jlabrams@ncsu.edu>
2021-10-06 22:07:13 -0400

    [Implementation] added error checking to ProductsReader

--- Project1/src/edu/ncsu/csc216/product_backlog/model/io/ProductsReader.java
+++ Project1/src/edu/ncsu/csc216/product_backlog/model/io/ProductsReader.java
@@ -62,6 +62,10 @@ public class ProductsReader {
 	private static Product processProduct(String productLine) {
 		try {
 			Scanner s = new Scanner(productLine);
+			if(s.nextLine().substring(0,1).equals("*")) {
+				s.close();
+				return null;
+			}
 			Product p = new Product(s.nextLine().substring(1));
 			s.useDelimiter("\\r?\\n?[*]");
 			while(s.hasNext()) {
@@ -119,5 +123,7 @@ public class ProductsReader {
 		}
 		
 	}
+	
+
 }
 
